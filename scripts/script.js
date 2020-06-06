a17927419c456f87ad57b6fe464908e544777b9b
satyam sinha <satyam8158@gmail.com>
2020-06-07 02:54:32 +0530

    first day work

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,16 @@
-
-
-window.onscroll=()=>{
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#navlinks").addClass("font-link-shrink");
-        $("#navlinks").removeClass("font-link");
-        $("#brand").addClass("font-shrink");
-        $("#brand").removeClass("font");
-      } else {
-        $("#navlinks").addClass("font-link");
-        $("#navlinks").removeClass("font-link-shrink");
-        $("#brand").addClass("font");
-        $("#brand").removeClass("font-shrink");
-      }
-}
+window.onscroll = () => {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    $("#navlinks").addClass("font-link-shrink");
+    $("#navlinks").removeClass("font-link");
+    $("#brand").addClass("font-shrink");
+    $("#brand").removeClass("font");
+  } else {
+    $("#navlinks").addClass("font-link");
+    $("#navlinks").removeClass("font-link-shrink");
+    $("#brand").addClass("font");
+    $("#brand").removeClass("font-shrink");
+  }
+};
+$('.nav-link').on('click',function() {
+  $('.navbar-collapse').collapse('hide');
+});
