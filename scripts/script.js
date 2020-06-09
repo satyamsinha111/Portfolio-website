7d927c858e89403a7ce10939fa9da25e4a014160
satyam sinha <satyam8158@gmail.com>
2020-06-09 16:37:47 +0530

    contact form backend part done

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,65 @@ window.onscroll = () => {
     $("#brand").removeClass("font-shrink");
   }
 };
-$('.nav-link').on('click',function() {
-  $('.navbar-collapse').collapse('hide');
+$(".nav-link").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
 });
+
+const sendMessage = (event) => {
+  //fetch from the api
+  event.preventDefault();
+  let firstName = document.getElementById("first-name").value;
+  let secondName = document.getElementById("second-name").value;
+  let companyName = document.getElementById("company-name").value;
+  let emailAddress = document.getElementById("email-address").value;
+  let message = document.getElementById("message").value;
+  var clientData = {
+    first_name: firstName,
+    second_name: secondName,
+    company_name: companyName,
+    email_address: emailAddress,
+    message: message,
+  };
+  console.log(JSON.stringify(clientData))
+  return fetch(`https://myportfolioapi123.herokuapp.com/client/message`, {
+    method: "POST",
+    
+    body: JSON.stringify(clientData),
+    headers: {
+      
+      "Content-Type": "application/json",
+    },
+    
+  })
+    .then((response) => {
+        if(response.status===200)
+        {
+          $("#failure").addClass("d-none");
+          $("#failure").removeClass("d-block");
+          $("#success").addClass("d-block");
+          $("#success").removeClass("d-none");
+        }
+        else{
+          $("#success").removeClass("d-block");
+          $("#success").addClass("d-none");
+          $("#failure").addClass("d-block");
+          $("#failure").removeClass("d-none");
+        }
+        setTimeout(()=>{
+          console.log("hello")
+          $("#success").removeClass("d-block")
+          $("#failure").removeClass("d-block")
+          $("#success").addClass("d-none");
+          $("#failure").addClass("d-none");
+        },3000)
+        setTimeout(()=>{
+          location.reload()
+        },3000)
+    })
+    .catch((error) => {
+      console.log(error)
+      return error;
+    });
+};
+
+$("#send").click(sendMessage);
